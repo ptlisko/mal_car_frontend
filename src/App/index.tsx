23d7e5b57ff99aace7773182fa049a4781848e2b
Ladislav Kostolny <kostolny.ladislav@merkatos.cz>
2022-08-05 23:54:12 +0200

    Base app changes

---
+++ src/App/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import './styles.css';
+
+const MalCarApp: React.FC = (): JSX.Element => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                Mal Car
+            </header>
+        </div>
+    );
+};
+
+export default MalCarApp;
